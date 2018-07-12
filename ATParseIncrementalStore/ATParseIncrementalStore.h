8dfe85062cb24cb70a2f779655b1a81110975cca
Nicolas Landa <nicolas.landa@ara-tech.es>
2018-07-12 12:54:44 +0200

    Initial Commit

---
+++ ATParseIncrementalStore/ATParseIncrementalStore.h
@@ -0,0 +1,19 @@
+//
+//  ATParseIncrementalStore.h
+//  ATParseIncrementalStore
+//
+//  Created by Nicolás Landa on 12/7/18.
+//  Copyright © 2018 Aratech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATParseIncrementalStore.
+FOUNDATION_EXPORT double ATParseIncrementalStoreVersionNumber;
+
+//! Project version string for ATParseIncrementalStore.
+FOUNDATION_EXPORT const unsigned char ATParseIncrementalStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATParseIncrementalStore/PublicHeader.h>
+
+
